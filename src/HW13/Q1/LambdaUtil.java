318a98cd9e817641e54ebedb8f9039d92cc9eb33
Gohartor <gohartour1417@gmail.com>
2025-04-05 00:31:13 +0330

    add HW13, Q1;

--- src/HW13/Q1/LambdaUtil.java
+++ src/HW13/Q1/LambdaUtil.java
@@ -1,44 +1,179 @@
 package HW13.Q1;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.Random;
+import java.util.function.*;
 
 public class LambdaUtil {
 
+    /**
+     * Returns {@link Supplier} that always supply "Hello"
+     *
+     * @return a string supplier
+     */
+    public static Supplier<String> helloSupplier() {
+        return () -> "Hello";
+    }
 
-    public static <T> T findFirst(List<T> list, Predicate<T> predicate) {
-        return list.stream()
-                .filter(predicate)
-                .findFirst()
-                .orElse(null);
+    /**
+     * Returns a {@link Predicate} of string that checks if string is empty
+     *
+     * @return a string predicate
+     */
+    public static Predicate<String> isEmptyPredicate() {
+        return s -> s.isEmpty();
+    }
+
+    /**
+     * Return a {@link Function} that accepts {@link String} and returns that string repeated n time, where n is passed
+     * as function argument
+     *
+     * @return function that repeats Strings
+     */
+    public static BiFunction<String, Integer, String> stringMultiplier() {
+//--------------01---------------
+//        return (string, integer) -> {
+//            StringBuilder result = new StringBuilder();
+//            for (int i = 0; i < integer; i++) {
+//                result.append(string);
+//            }
+//            return result.toString();
+//        };
+//--------------02---------------
+//        return (string, integer) -> String.valueOf(string).repeat(Math.max(0, integer));
+//--------------03---------------
+//        return (string, integer) -> string.repeat(integer);
+//--------------04---------------
+        return String::repeat;
+    }
+
+    /**
+     * Returns a {@link Function} that converts a {@link BigDecimal} number into a {@link String} that start with
+     * a dollar sign and then gets a value
+     *
+     * @return function that converts adds dollar sign
+     */
+    public static Function<BigDecimal, String> toDollarStringFunction() {
+        return bd ->  bd.toString() + " $";
+    }
+
+    /**
+     * Receives two parameter that represent a range and returns a {@link Predicate<String>} that verifies if string
+     * length is in the specified range. E.g. min <= length < max
+     *
+     * @param min min length
+     * @param max max length
+     * @return a string predicate
+     */
+    public static Predicate<String> lengthInRangePredicate(int min, int max) {
+        return S -> S.length() >= min && S.length() < max;
+    }
+
+    /**
+     * Returns a {@link Supplier} of random integers
+     *
+     * @return int supplier
+     */
+    public static IntSupplier randomIntSupplier() {
+//        return (int) Math.random()*100;
+        Random rn = new Random();
+//        return () -> rn.nextInt();
+        return rn::nextInt;
     }
 
 
-    public static <T, R> List<R> map(List<T> list, Function<T, R> function) {
-        return list.stream()
-                .map(function)
-                .toList();
+    /**
+     * Returns an {@link IntUnaryOperator} that receives an int as a bound parameter, and returns a random int
+     *
+     * @return int operation
+     */
+    public static IntUnaryOperator boundedRandomIntSupplier() {
+        Random random = new Random();
+        return bound -> random.nextInt(bound);
+//        return random::nextInt;
     }
 
-    public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
-        return list.stream()
-                .filter(predicate)
-                .toList();
+    /**
+     * Returns {@link IntUnaryOperator} that calculates an integer square
+     *
+     * @return square operation
+     */
+    public static IntUnaryOperator intSquareOperation() {
+        return x -> x * x;
     }
 
-    public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    /**
+     * Returns a {@link LongBinaryOperator} sum operation.
+     *
+     * @return binary sum operation
+     */
+    public static LongBinaryOperator longSumOperation() {
+        return (l1, l2) -> l1 + l2;
+//        return Long::sum;
+    }
 
-        Integer firstEven = findFirst(numbers, n -> n % 2 == 0);
-        System.out.println("first even: " + firstEven);
+    /**
+     * Returns a {@link ToIntFunction<String>} that converts string to integer.
+     *
+     * @return string to int converter
+     */
+    public static ToIntFunction<String> stringToIntConverter() {
+        return s -> Integer.parseInt(s);
+//        return Integer::parseInt;
+    }
 
-        List<Integer> squared = map(numbers, n -> n * n);
-        System.out.println("squared: " + squared);
+    /**
+     * Receives int parameter n, and returns a {@link Supplier} that supplies {@link IntUnaryOperator}
+     * that is a function f(x) = n * x
+     *
+     * @param n a multiplier
+     * @return a function supplier
+     */
+    public static Supplier<IntUnaryOperator> nMultiplyFunctionSupplier(int n) {
+        return () -> x -> x * n;
+    }
 
-        List<Integer> evenNumbers = filter(numbers, n -> n % 2 == 0);
-        System.out.println("all even numbers: " + evenNumbers);
+    /**
+     * Returns {@link Supplier} of {@link Supplier} of {@link Supplier} of {@link String} "WELL DONE".
+     *
+     * @return a supplier instance
+     */
+    public static Supplier<Supplier<Supplier<String>>> trickyWellDoneSupplier() {
+        return () -> (() -> (() -> "WELL DONE"));
+    }
+
+    /**
+     * Returns a {@link UnaryOperator} that accepts str to str function and returns the same function composed with trim
+     *
+     * @return function that composes functions with trim() function
+     */
+    public static UnaryOperator<Function<String, String>> composeWithTrimFunction() {
+        return u -> u.compose(s -> s.trim());
+//        return u -> u.compose(String::trim);
+    }
+
+    /**
+     * Returns a {@link BiFunction} that has two parameters. First is {@link IntUnaryOperator} which is some integer function.
+     * Second is {@link IntPredicate} which is some integer condition. And the third is {@link IntUnaryOperator} which is
+     * a new composed function that uses provided predicate (second parameter of binary function) to verify its input
+     * parameter. If predicate returns {@code true} it applies a provided integer function
+     * (first parameter of binary function) and returns a result value, otherwise it returns an element itself.
+     *
+     * @return a binary function that receiver predicate and function and compose them to create a new function
+     */
+    public static BiFunction<IntUnaryOperator, IntPredicate, IntUnaryOperator> functionToConditionalFunction() {
+        return (func, pre) -> x -> pre.test(x) ? func.applyAsInt(x) : x;
+    }
+
+    /**
+     * Returns a {@link BiFunction} which first parameter is a {@link Map} where key is a function name, and value is some
+     * {@link IntUnaryOperator}, and second parameter is a {@link String} which is a function name. If the map contains a
+     * function by a given name then it is returned by high order function otherwise an identity() is returned.
+     *
+     * @return a high-order function that fetches a function from a function map by a given name or returns identity()
+     */
+    public static BiFunction<Map<String, IntUnaryOperator>, String, IntUnaryOperator> functionLoader() {
+        return (map, s) -> map.getOrDefault(s, IntUnaryOperator.identity());
     }
 }
